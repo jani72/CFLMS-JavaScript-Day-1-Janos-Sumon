9cccb5741bf82ccf6c8b02d3d01f549091d07c4b
Hans-Jürgen Janos <deejayjuergen@live.at>
2020-05-25 13:12:21 +0200

    Intermediate Exercise

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ console.log("Hi my Name is Martin")
 console.log("and I am "+age+" Years old");
 console.log("The most valuable player of the match is " +team [2]);
 console.log(cars.sort());
+console.log(fruits);
+console.log(animals.sort());
+console.log(animals.join("\n"));
