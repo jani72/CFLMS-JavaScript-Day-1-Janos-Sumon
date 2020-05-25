f5191a99aa31c8c38ef7a95a7838704aa3855d1b
Harprit Sumon <harpritsmn32@yahoo.de>
2020-05-25 15:28:10 +0200

    added advanced exercise

---
+++ advanced/advanced.js
@@ -0,0 +1,45 @@
+var a = 7;
+var b = 14;
+var c = "21";
+var d = '36';
+var e = 42;
+
+var c = parseFloat("21")
+var d = parseFloat("36")
+
+var add = a+b+e+c+d;
+
+console.log(add);
+
+
+var f = '1';
+var g = 15;
+var h = 8;
+var i = "1";
+
+var f = parseFloat("1")
+var i = parseFloat("1")
+
+var multiplication = f*g*h*i;
+
+console.log(multiplication);
+
+document.write(add/multiplication);
+
+//Second Exercise
+
+hallo=[ 
+[1, 2, 1, 24], 
+[8, 11, 9, 4], 
+[7, 0, 7, 27], 
+[7, 4, 28, 14], 
+[3, 10, 25, 7], 
+[21, 4, 6, 17], 
+[3, 5, 26, 3] 
+];
+
+console.log(hallo[1][1]);
+console.log(hallo[4][2]);
+console.log(hallo[5][3]);
+console.log(hallo[2][3]);
+console.log(hallo[2][1]);
--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+console.log("Hello from the Console");
+//document.write("Hello from the Browser");
+
+var age = 25;
 console.log("Hi my Name is Martin")
-console.log("and I am "+age+" Years old");
+console.log("and I am " +age+ " Years old");
+
+team =["Martin","Thomas","Peter","Mathias","Niki"];
 console.log("The most valuable player of the match is " +team [2]);
+
+var cars =["Tesla","Audi","Renault","Volvo","Mazda","Fiat","Ferrari"];
+console.log(cars);
 console.log(cars.sort());
+
+
+var animals=['monkey','horse','dog'];
+document.write(animals.join("<br>"));
+
+
+
